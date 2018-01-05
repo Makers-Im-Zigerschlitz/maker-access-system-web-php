861986addae8cad17a69c958bcac6a3580756d0c
Mike Zweifel <mike@mikezweifel.ch>
2018-01-05 13:12:01 +0100

    Error handling in login_action angepasst

--- WebService/portal/login/login_action.php
+++ WebService/portal/login/login_action.php
@@ -2,11 +2,16 @@
 session_start();
 include "../config/config.inc.php";
 $sqlconn = mysqli_connect($mysqlhost,$mysqluser,$mysqlpass,$mysqldb);
-echo mysql_error();
 $username = $_POST["username"];
 $password = md5($_POST["password"]);
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    //you need to exit the script, if there is an error
+    exit();
+}
 $query = "SELECT * FROM $mysqldb.tblUsers WHERE username LIKE '$username' AND password = '$password';";
-// echo $query;
+//echo $query;
 $result = mysqli_query($sqlconn,$query);
 $sqlfetch = mysqli_fetch_array($result);
 
