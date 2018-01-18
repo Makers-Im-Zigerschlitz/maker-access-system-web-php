4eb95b9ccbcedb2734d2b1cf0a1539dd060c3bbe
Mike Zweifel <mike@mikezweifel.ch>
2018-01-18 22:49:12 +0100

    Corrected last commit

--- WebService/portal/admin/actions/createpost.php
+++ WebService/portal/admin/actions/createpost.php
@@ -9,10 +9,10 @@ include "../../config/config.inc.php";
 include "../../includes/dictionary.inc.php";
 include "../../includes/functions.inc.php";
 $db = new PDO('mysql:host=localhost;dbname='.$mysqldb, $mysqluser, $mysqlpass);
-$stmt = $db->prepare("INSERT INTO tblNews(title,text,author) VALUES (:title,:text,:author)");
-$stmt->bindValue(':title', $_POST["posttitle"], PDO::PARAM_STR);
-$stmt->bindValue(':deviceDesc', $_POST["text"], PDO::PARAM_STR);
-$stmt->bindValue(':author', $_SESSION["username"], PDO::PARAM_STR);
+$stmt = $db->prepare("INSERT INTO tblNews (title,text,author) VALUES (?,?,?)");
+$stmt->bindValue(1, $_POST["posttitle"], PDO::PARAM_STR);
+$stmt->bindValue(2, $_POST["text"], PDO::PARAM_STR);
+$stmt->bindValue(3, $_SESSION["username"], PDO::PARAM_STR);
 $stmt->execute();
 
 if ($stmt->rowCount()>0) {
