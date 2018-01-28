95d5cbd2d99560c564721c163f64fb57a6cc1fee
Mike Zweifel <mike@mikezweifel.ch>
2018-01-28 13:15:22 +0100

    Dictionary Template prepared

---
+++ WebService/portal/includes/dictionary_template.php
@@ -0,0 +1,68 @@
+<?php
+$dict = array();
+
+//General
+$dict["Gen_Welcome"] = "Welcome";
+
+//Login
+$dict["Login_Prompt"] = "Please log in";
+$dict["Login_Logon"] = "Log on";
+$dict["Login_Logout"] = "Logout";
+$dict["Login_Username"] = "Username";
+$dict["Login_Passwort"] = "Password";
+$dict["Login_Auth_Rejected_Short"] = "Authentication rejected";
+$dict["Login_Auth_Rejected_Long"] = "Your Authentication was rejected! Maybe you are not logged in or you Username / Password was wrong";
+$dict["Login_New_Password"] = "New Password";
+$dict["Login_Repeat_Password"] = "Repeat Password";
+$dict["Login_Change_Password"] = "Change Password";
+$dict["Login_Password_Change_Success"] = "Your password was successfully changed!";
+$dict["Login_Change_Lang"] = "Change Language";
+
+//Navigation
+$dict["Nav_Return_Home"] = "Go back to Home";
+$dict["Nav_Members"] = "Members";
+$dict["Nav_News"] = "News";
+$dict["Nav_Documents"] = "Documents";
+$dict["Nav_Admin_Panel"] = "Admin";
+$dict["Nav_Access_System"] = "Accessystem";
+$dict["Nav_Settings"] = "Settings";
+
+//Users
+$dict["User_Create_Success"] = "User successfully created";
+$dict["User_Delete_Success"] = "User has been deleted";
+$dict["User_Surname"] = "Surname";
+$dict["User_Lastname"] = "Lastname";
+$dict["User_Birthday"] = "Birthday";
+$dict["User_Phone"] = "Phone";
+$dict["User_Mail"] = "Mail Adress";
+$dict["User_Create"] = "Create User";
+$dict["User_Delete"] = "Delete User";
+
+//Documents and Files
+$dict["Doc_Upload_Document"] = "Upload Document";
+$dict["Doc_Filename"] = "Filename";
+$dict["Doc_Upload"] = "Upload";
+$dict["Doc_Open_File"] = "Open File";
+$dict["Doc_Title"] = "Title";
+$dict["Doc_Overview"] = "Document overview";
+$dict["Doc_Delete_Files"] = "Delete files";
+$dict["Doc_Upload_Success"] = "File successfully uploaded";
+$dict["Doc_Upload_Filetype_Denied"] = "The file with the uploaded extension is not allowed";
+$dict["Doc_File_Exists"] = "This file already exists!";
+
+//News
+$dict["News_Create_Entry"] = "Create Newsentry";
+$dict["News_Title"] = "News title";
+$dict["News_Text"] = "News text";
+$dict["Post_Text"] = "Post text";
+$dict["Post_Posts"] = "Posts";
+$dict["Post_Delete_Post"] = "Delete Post";
+
+//Devices 
+$dict["Dev_Name"] = "Devicename";
+$dict["Dev_Description"] = "Device description";
+$dict["Dev_Delete"] = "Delete device";
+$dict["Dev_Devices"] = "Devices";
+$dict["Dev_ID"] = "Device ID";
+$dict["Dev_Create"] = "Create device";
+?>
