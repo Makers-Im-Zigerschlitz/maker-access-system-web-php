fa9b040879194ad14c78e72cf6e9eeff9ca0bbc5
Mike Zweifel <mike@mikezweifel.ch>
2018-03-31 01:08:49 +0200

    Integrated log to admin panel. Fixes #16.

--- WebService/portal/admin/index.php
+++ WebService/portal/admin/index.php
@@ -12,6 +12,7 @@
   include "../config/config.inc.php";
   include "../includes/dictionary.$language.inc.php";
   include "../includes/functions.inc.php";
+  include "../includes/categories.inc.php";
   $db = new PDO('mysql:host=' . $mysqlhost . ';dbname=' . $mysqldb, $mysqluser, $mysqlpass);
   $sqlconn = mysqli_connect($mysqlhost, $mysqluser, $mysqlpass, $mysqldb);
   ?>
@@ -19,7 +20,8 @@
   <link rel="stylesheet" href="../css/normalize.css" type="text/css">
   <link rel='stylesheet prefetch' href='css/font-awesome.min.css'>
   <link rel="stylesheet" href="css/style.css" type="text/css">
-
+  
+  <script src="../js/logViewer.js"></script>
   <script src="js/prefixfree.min.js"></script>
     <?php
     if (!isset($_GET["site"])) {
@@ -92,7 +94,73 @@
   ?>
   <?php if ($_GET["site"] == "logs"): ?>
     <section class="panel important">
-      <iframe src="show_log.php" width="100%" height="500px"></iframe>
+      <table class="table-fill" id="logbook">
+      <thead>
+        <tr>
+          <th class="text-left">Status</th>
+          <th class="text-left">Log ID</th>
+          <th class="text-left">Timestamp</th>
+          <th class="text-left">Action</th>
+          <th class="text-left">User</th>
+          <th class="text-left">Device ID</th>
+          <th class="text-left">IP Address</th>
+		  <th class="text-left">
+            Category
+              <select id="catSelect" onchange="catChange();" name="category" value="Unknown">
+                <option value="all">All</option>
+                <?php
+                  foreach ($categories as $key => $value) {
+                    if (isset($_GET["cat"])) {
+                      if ($_GET["cat"] == $key) {
+                        echo "<option selected='selected' value='$key'>$value</option>";
+                      }
+                      else {
+                        echo "<option value='$key'>$value</option>";
+                      }
+                    }
+                    else {
+                      echo "<option value='$key'>$value</option>";
+                    }
+                  }
+                 ?>
+              </select>
+          </th>
+        </tr>
+      </thead>
+    <?php
+    $stmt = $db->query('SELECT uid,username FROM tblUsers');
+    while ($temp = $stmt->fetch(PDO::FETCH_ASSOC)) {
+          $members[$temp["uid"]] = $temp["username"];
+      }
+      if (!isset($_GET["cat"])) {
+        $stmt = $db->query('SELECT * FROM tblLogs ORDER BY logID DESC');
+      }
+      else {
+        $tempcat = $_GET["cat"];
+        $stmt = $db->query('SELECT * FROM tblLogs WHERE logCategory = '.$tempcat.' ORDER BY logID DESC');
+      }
+
+      echo "<tbody class='table-hover'>";
+        while ($logentry = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        if (strpos($logentry["action"], "Error") !== false) {
+            echo "<td><img class='status' src='../img/error.png' alt=''></td>";
+        }
+        else {
+          echo "<td><img class='status' src='../img/ok.png' alt=''></td>";
+        }
+        echo "<td class='text-left'>".$logentry["logID"]."</td>";
+        echo "<td class='text-left'>".$logentry["timestamp"]."</td>";
+        echo "<td class='text-left'>".$logentry["action"]."</td>";
+        echo "<td class='text-left'>".$members[$logentry["uid"]]."</td>";
+        echo "<td class='text-left'>".$logentry["deviceID"]."</td>";
+        echo "<td class='text-left'>".$logentry["r_host"]."</td>";
+        echo "<td class='text-left'>".$categories[$logentry["logCategory"]]."</td>";
+        echo "</tr>";
+      }
+     ?>
+     </tbody>
+     </table>
     </section>
 <?php endif; ?>
 <?php if ($_GET["site"] == "users"): ?>
--- WebService/portal/js/logViewer.js
+++ WebService/portal/js/logViewer.js
@@ -2,11 +2,12 @@ function catChange() {
   var e = document.getElementById("catSelect");
   var value = e.options[e.selectedIndex].value;
   var text = e.options[e.selectedIndex].text;
+  var path = document.location.href;
   if (value != "all") {
-    var path = document.location.pathname + "?cat=" + value;
+		path = document.location.pathname + "?site=logs&cat=" + value;
   }
   else {
-    var path = document.location.pathname;  
+    path = document.location.pathname;  
   }
   window.location.replace(path);
 }
